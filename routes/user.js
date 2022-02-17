507e6cb27dab3289e31de3efa65b7f8795571b91
antoine6191 <59445232+antoine6191@users.noreply.github.com>
2022-02-17 09:54:38 +0100

    init

--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,11 @@ const ressourceController = require('../controllers/ressource')
 
 const router = express.Router();
 
-router.get('/user/:userId', userController.getUser)
-router.post('/user/add-user', userController.postUser)
+router.get('/user/:userId', userController.getUser) //
+router.post('/user/add-user', userController.postUser) //
 
-router.get('/ressources', ressourceController.getRessources)
-router.get('/ressource/:ressourceId', ressourceController.getRessource)
-router.post('/ressources/add-ressource', ressourceController.postRessource)
+router.get('/ressources', ressourceController.getRessources) //
+router.get('/ressource/:ressourceId', ressourceController.getRessource) //
+router.post('/ressources/add-ressource', ressourceController.postRessource) //
 
 module.exports = router;
