4cc8e0c586451bcd7242c47e8e74560169adb5e1
louis <louis@LOUIS-4.home.local>
2022-04-14 11:47:56 +0200

    Intégration des variables d'environnement

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('ressource_relationelle', process.env.USERBDD, process.env.MDPBDD, {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: 8889
+const sequelize = new Sequelize('ressource_relationelle', process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: process.env.DB_CONTEXT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
   //debug: true
 });
 
