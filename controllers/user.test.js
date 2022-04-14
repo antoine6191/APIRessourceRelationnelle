081bea75a56a2cf430e324f249e30826f1ae1658
antoine6191 <59445232+antoine6191@users.noreply.github.com>
2022-04-14 16:26:15 +0200

    user@statistiqueTest

---
+++ controllers/user.test.js
@@ -0,0 +1,19 @@
+const Axios = require('axios');
+const User = require('../models/user')
+let userId = 3
+let nomTest = 'user';
+let i = 0;
+
+test(nomTest+i+1+' : récupérer le nombre de user', async () => {
+    const response = await Axios.get('http://localhost:3000/admin/users')
+    //console.log(response)
+    expect(response.data.length).toEqual(4);
+});
+
+
+test(nomTest+i+1+' : récupérer le user ayant l id'+userId, async () => {
+    const response = await Axios.get('http://localhost:3000/user/'+userId)
+    //console.log(response)
+    expect(response.data.nom).toEqual("poyeau");
+});
+
